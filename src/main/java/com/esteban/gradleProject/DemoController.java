46317595a0410e42c2bd871dee3e7f6fe4310ebb
Vicente San silvestre <lema017@gmail.com>
2021-12-16 08:24:48 -0600

    changes

---
+++ src/main/java/com/esteban/gradleProject/DemoController.java
@@ -0,0 +1,14 @@
+package com.esteban.gradleProject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( value = "/api/v1")
+public class DemoController {
+
+    @RequestMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
